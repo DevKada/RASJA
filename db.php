309d6a91b8e1e94e8b28736ae821ef96a2a46bd8
Shenhelle Ashley Guantes <shenhelleashley@gmail.com>
2025-06-26 04:27:21 +0800

    update

--- db.php
+++ db.php
@@ -4,18 +4,14 @@ $user = "root";
 $password = "";
 $database = "jobCircle";
 
-// Enable detailed MySQLi error reporting
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-// Create connection
 $conn = mysqli_connect($host, $user, $password, $database);
 
-// Check connection
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
 
-// Reusable query function (supports prepared statements)
 function executeQuery($query, $params = [])
 {
   global $conn;
